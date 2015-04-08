7646f9591eaf8b30dfb481aa4dd3a081d8ed1177
Wanping Lee <wanping.lee@sbgenomics.com>
2015-04-08 12:18:33 -0400

    Revise the error message when the registration info cannot be found. 	modified:   upload_abstract.php 	modified:   upload_cv.php

--- HTML/upload_cv.php
+++ HTML/upload_cv.php
@@ -20,7 +20,7 @@ $result = mysqli_query($conn, $query);
 if (mysqli_num_rows($result) == 0) {
   // Error message
   echo '<script language="javascript"> 
-  alert("ERROR: We cannot comfirm your registraction. Please check your EventBrite order number and Email, and complete the registraction form before submitting the C.V."); 
+  alert("Sorry, we can not verify your registration. Please make sure the same order # and email that you used for your registration."); 
   window.location.replace("http://btbatw.org/2015/#register");
   </script>';
 } else {
