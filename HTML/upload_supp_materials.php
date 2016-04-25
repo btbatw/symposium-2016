07e81972e1aee652c47a9350d7d0052a71b3f618
Chiao-Feng Lin <chiaofeng.lin@gmail.com>
2016-04-25 00:51:10 -0400

    modified supp_materials form and upload

--- HTML/upload_supp_materials.php
+++ HTML/upload_supp_materials.php
@@ -15,7 +15,7 @@ $teamname  = addslashes($_POST['teamname']);
 $email        = addslashes($_POST['email']);
 
 // Check the uniqueness of event_code
-$query = "SELECT * FROM sym_entrepr WHERE sym_entrepr_teamname='$teamname' AND email='$email'";
+$query = "SELECT * FROM sym_entrepr WHERE sym_entrepr_teamname='$teamname' AND sym_entrepr_email='$email'";
 $result = mysqli_query($conn, $query);    
 if (mysqli_num_rows($result) == 0) {
   // Error message
@@ -26,8 +26,8 @@ if (mysqli_num_rows($result) == 0) {
 } else {
   // Upload CV
   if((!empty($_FILES["materials"])) && ($_FILES['materials']['error'] == 0)) {
-    $filename = basename($_FILES['cv']['name']);
-    $newname = 'cv/' . $teamname . '.' . $filename;
+    $filename = basename($_FILES['materials']['name']);
+    $newname = 'materials/' . $teamname . '.' . $filename;
 
     $ext = pathinfo($filename, PATHINFO_EXTENSION);
     if($ext !== 'pdf') {
@@ -36,32 +36,26 @@ if (mysqli_num_rows($result) == 0) {
       window.location.replace("http://btbatw.org/2015/#register"); 
       </script>';
     } else {
-      if ((move_uploaded_file($_FILES['cv']['tmp_name'],$newname))) {
-	$sql = "UPDATE sym_registrant SET ".
-               "cv = '$newname', ".
-	       "com1_prefer = '$_POST[com1_prefer]', ".
-	       "com2_prefer = '$_POST[com2_prefer]', ".
-	       "com3_prefer = '$_POST[com3_prefer]', ".
-	       "com4_prefer = '$_POST[com4_prefer]', ".
-	       "com5_prefer = '$_POST[com5_prefer]', ".
-               "com6_prefer = '$_POST[com6_prefer]' ".
-               "WHERE event_code = '$event_code' AND email = '$email'";
+      if ((move_uploaded_file($_FILES['materials']['tmp_name'],$newname))) {
+	$sql = "UPDATE sym_entrepr SET ".
+               "materials = '$newname', ".
+               "WHERE sym_entrepr_teamname='$teamname' AND sym_entrepr_email='$email'";
   
         if (mysqli_query($conn, $sql)) {
           echo '<script language="javascript"> 
-          alert("Thank you so much for submitting the C.V."); 
-          window.location.replace("http://btbatw.org/2015/#register"); 
+          alert("Thank you so much for submitting Supporting Materials."); 
+          window.location.replace("http://btbatw.org/2025/#register"); 
           </script>';
         } else {
           echo '<script language="javascript"> 
-          alert("Error: We cannot process your abstract submission. Please contact us."); 
-          window.location.replace("http://btbatw.org/2015/#register"); 
+          alert("Error: We cannot process your Supporting Materials. Please contact us."); 
+          window.location.replace("http://btbatw.org/2025/#register"); 
           </script>';
         }
       } else {
         echo '<script language="javascript"> 
-        alert("Error: We cannot upload your C.V. Please contact us."); 
-        window.location.replace("http://btbatw.org/2015/#register"); 
+        alert("Error: We cannot upload your Supporting Materials. Please contact us."); 
+        window.location.replace("http://btbatw.org/2025/#register"); 
         </script>';
       }
     }
