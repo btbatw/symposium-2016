4726c1962830d673ac60a3760f0576a2965fa034
Chiao-Feng Lin <chiaofeng.lin@gmail.com>
2016-04-26 23:46:21 -0400

    register entrepreneur presentation form and upload php

--- HTML/upload_reg.php
+++ HTML/upload_reg.php
@@ -13,14 +13,13 @@ if (!$conn) {
 
 $teamname  = addslashes($_POST['teamname']);
 $email        = addslashes($_POST['email']);
-$phone = $_POST['phone'];
+//$phone = $_POST['phone'];
 $innovation        = addslashes($_POST['innovation']);
 $development        = addslashes($_POST['development']);
 $market        = addslashes($_POST['market']);
 $intellectual        = addslashes($_POST['intellectual']);
 $background  = addslashes($_POST['background']);
 $future  = addslashes($_POST['future']);
-$materials = $_POST['materials'];
 
 // Check the uniqueness of event_code
 $query = "SELECT * FROM sym_entrepr WHERE sym_entrepr_teamname='$teamname'";
@@ -34,22 +33,32 @@ if (mysqli_num_rows($result) > 0) {
   </script>';
 } else {
 
-	$sql = "INSERT INTO sym_entrepr (sym_entrepr_id, sym_entrepr_teamname, sym_entrepr_email, sym_entrepr_phone, sym_entrepr_innovation, sym_entrepr_development, sym_entrepr_market, sym_entrepr_intellectual, sym_entrepr_background, sym_entrepr_future, sym_entrepr_material)
-		VALUES ('default', '$teamname', '$email', '$phone', '$innovation', '$development', '$market', '$intellectual', '$background', '$future', '$_POST[materials]')";
+	if((!empty($_FILES["materials"])) && ($_FILES['materials']['error'] == 0)) {
+		$filename = basename($_FILES['materials']['name']);
+    $newname = 'materials/' . $teamname . '.' . $filename;
 
-
-  if (mysqli_query($conn, $sql)) {
-    echo '<script language="javascript"> 
-    alert("Thank you so much for the registration."); 
-    window.location.replace("http://btbatw.org/2025/#register"); 
-    </script>';
-  } else {
-    echo '<script language="javascript"> 
-    alert("Error: We cannot process your registraction. Please contact us."); 
-    window.location.replace("http://btbatw.org/2016/#register"); 
-    </script>';
-  }
-
+		if ((move_uploaded_file($_FILES['materials']['tmp_name'],$newname))) {
+			$sql = "INSERT INTO sym_entrepr (sym_entrepr_id, sym_entrepr_teamname, sym_entrepr_email, sym_entrepr_innovation, sym_entrepr_development, sym_entrepr_market, sym_entrepr_intellectual, sym_entrepr_background, sym_entrepr_future, sym_entrepr_material)
+			VALUES ('default', '$teamname', '$email', '$innovation', '$development', '$market', '$intellectual', '$background', '$future', '$newname')";
+			
+			if (mysqli_query($conn, $sql)) {
+				echo '<script language="javascript">
+				alert("Thank you so much for the registration."); 
+				window.location.replace("http://btbatw.org/2025/#register"); 
+				</script>';
+			} else {
+				echo '<script language="javascript"> 
+				alert("Error: We cannot process your registraction. Please contact us."); 
+				window.location.replace("http://btbatw.org/2016/#register"); 
+				</script>';
+			}
+		} else {
+				echo '<script language="javascript"> 
+				alert("Error: We cannot process your registraction. Please contact us."); 
+				window.location.replace("http://btbatw.org/2016/#register"); 
+				</script>';
+		}
+	}
 }
 
 mysqli_close($conn);
