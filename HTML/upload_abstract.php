f9eaeabdc3f28efba2084f4bbb26e0108cda8548
Wan-Ping Lee <wanping.lee@gmail.com>
2016-04-27 20:02:58 -0400

    Add name and aff in abstract form

--- HTML/upload_abstract.php
+++ HTML/upload_abstract.php
@@ -15,6 +15,8 @@ $email    = addslashes($_POST['email']);
 $title    = addslashes($_POST['title']);
 $field    = $_POST['field'];
 $talk     = $_POST['talk'];
+$name     = addslashes($_POST['name']);
+$aff      = addslashes($_POST['aff']);
 
 if((!empty($_FILES['abstract'])) && ($_FILES['abstract']['error'] == 0)) {
 	$filename = basename($_FILES['abstract']['name']);
@@ -27,7 +29,7 @@ if((!empty($_FILES['abstract'])) && ($_FILES['abstract']['error'] == 0)) {
                 </script>';
         } else {
                 if ((move_uploaded_file($_FILES['abstract']['tmp_name'],$newname))) {
-                        $sql = "INSERT INTO abstracts (email, title, field, talk, filename) VALUES ('$email', '$title', '$field', '$talk', '$newname')";
+                        $sql = "INSERT INTO abstracts (email, title, field, talk, filename, name, aff) VALUES ('$email', '$title', '$field', '$talk', '$newname', '$name', '$aff')";
 	
                         if (mysqli_query($conn, $sql)) {
                                 echo '<script language="javascript"> 
